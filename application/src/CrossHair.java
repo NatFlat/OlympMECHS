99c30304388dcaa9dc2d35cea1b2fe1b605db25f
Nathaniel <flatenn@myumanitoba.ca>
2019-04-27 16:19:47 -0500

    mergeBranch

--- application/src/CrossHair.java
+++ application/src/CrossHair.java
@@ -1,2 +1,20 @@
-public class CrossHair {
+import processing.core.PApplet;
+
+public class CrossHair extends GameObject
+{
+    protected int posX;
+    protected int posY;
+    protected int direction;
+
+    public CrossHair(PApplet papp)
+    {
+        posX=papp.width/2;
+        posY=papp.height/2;
+        direction = 0;
+    }
+
+    void setup()
+    {
+
+    }
 }
