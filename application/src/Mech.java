4f086311ca4cc77585542c2ac5c8d0fe4e2e92a8
MandatorySpaghetti <goulddunlop@gmail.com>
2019-04-27 12:38:48 -0500

    gameobject and resource directory

---
+++ application/src/Mech.java
@@ -0,0 +1,16 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Mech extends GameObject
+{
+	public Mech(String spriteFilePath)
+	{
+		loadImage(spriteFilePath);
+	}
+
+	//TODO: Stats, Getters/Setters, Input Listeners, Update Logic
+
+}
