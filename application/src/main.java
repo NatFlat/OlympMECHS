ae4254e1d1bceaf8569174358cb947ae7b401551
Nathaniel <flatenn@myumanitoba.ca>
2019-04-27 11:53:30 -0500

    imi

--- application/src/main.java
+++ application/src/main.java
@@ -1,3 +1,14 @@
-public class main
+import processing.core.*;
+import java.awt.event.*;
+import java.util.List;
+import java.util.ListIterator;
+
+public class main extends PApplet
 {
+    //public GameLogic game;
+    //public PlayerMech player;
+    //public EnemyMech enemy;
+    public List<GameObject> gameObjects;
+    public ListIterator<GameObject> iterator;
+    GameObject current;
 }
